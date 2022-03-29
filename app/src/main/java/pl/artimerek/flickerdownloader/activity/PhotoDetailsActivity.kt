fe409d6335a2c071a154d30865af3e155a607f4b
Konrad Rzepliński <konradbiznesu@gmail.com>
2022-03-29 18:12:32 +0200

    chaning themes

--- app/src/main/java/pl/artimerek/flickerdownloader/activity/PhotoDetailsActivity.kt
+++ app/src/main/java/pl/artimerek/flickerdownloader/activity/PhotoDetailsActivity.kt
@@ -2,8 +2,11 @@ package pl.artimerek.flickerdownloader.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_photo_details.*
 import pl.artimerek.flickerdownloader.R
+import pl.artimerek.flickerdownloader.model.Photo
 
 class PhotoDetailsActivity : AppCompatActivity() {
 
@@ -12,6 +15,18 @@ class PhotoDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
         setSupportActionBar(toolbar)
+
+        val photo = intent.getParcelableExtra<Photo>(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+
+        Picasso.with(this)
+            .load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
     }
 
     override fun onSupportNavigateUp(): Boolean {
